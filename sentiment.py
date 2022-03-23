19f64329b67616f3a1209c5e17be9c89b0f024b3
Mohammad <aali361@gmail.com>
2022-03-23 17:01:20 +0430

    feat: minor change

--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,6 @@ from transformers import pipeline
 
 # sentiment
 text = "سیب‌زمینی بی‌کیفیت بود."
-sentiment_model = pipeline('sentiment-analysis', model='/app/model')
+sentiment_model = pipeline('sentiment-analysis', model='/app/model', return_all_scores=True)
 result = sentiment_model(text)
 print(result)
