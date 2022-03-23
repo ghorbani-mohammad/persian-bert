747b21b3a44a64a8d506be220e5d2a20c304ea67
Mohammad <aali361@gmail.com>
2022-03-23 14:43:21 +0430

    feat: minor change

--- sentiment.py
+++ sentiment.py
@@ -1,4 +1,6 @@
 from transformers import pipeline
+from transformers import AutoTokenizer
 
 # sentiment
+tokenizer = AutoTokenizer.from_pretrained('/app/model')
 sentiment_model = pipeline('sentiment-analysis', model='/app/model')
